d660bcefcba4c7c7a2e3f5f3a8a08583fc3dad9a
saketh92 <saisakethnandagiri@gmail.com>
2017-04-12 21:06:59 -0400

    vBoard first commit

---
+++ src/test/java/com/project/www/UdemyProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.project.www;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UdemyProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
